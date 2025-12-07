c522664e024f6aea1ffa6a6fd7ed425ca30e7c3d
Abdulrahmankhan05 <abdulkasim901@gmail.com>
2025-12-07 23:54:31 +0530

    Added the Book class for storing details of the book

---
+++ src/model/Book.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Book {
+    private String id;
+    private String title;
+    private String author;
+    private Boolean availability;
+
+    public Book(String id, String title, String author){
+        this.id=id;
+        this.title= title;
+        this.author=author;
+        this.availability=true;
+    }
+    public String getId(){
+        return id;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public String getAuthor(){
+        return author;
+    }
+    public void markAsBorrowed(){
+        availability=false;
+    }
+
+    public void markAsReturned(){
+        availability=true;
+    }
+    public Boolean isAvailable(){
+        return availability;
+    }
+}
