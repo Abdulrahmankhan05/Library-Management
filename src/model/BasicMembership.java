9381828aa63c181469c62eba6cf411f0a2e255a0
Abdulrahmankhan05 <abdulkasim901@gmail.com>
2025-12-07 23:43:15 +0530

    Added the Basic membership class

--- src/model/BasicMembership.java
+++ src/model/BasicMembership.java
@@ -1,4 +1,13 @@
 package model;
 
-public class BasicMembership {
+public class BasicMembership extends Membership{
+    @Override
+    public int getBorrowlimit(){
+        return 2;
+    }
+
+    @Override
+    public String getTypeName() {
+        return "BASIC";
+    }
 }
